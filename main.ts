57d7cd70d63e3d89d59c956d32074d009506b047
Blobbey2000 <116673732+Blobbey2000@users.noreply.github.com>
2023-03-29 15:01:01 -0700

    Changed code namespace

--- main.ts
+++ main.ts
@@ -1,1 +1,35 @@
+namespace scene {
 
+    let canCameraScroll = 0
+    let horizontalSpeed = 0
+    let verticalSpeed = 0
+
+    /**
+    * Stops camera scroll.
+    */
+    //% block
+    //% group="Camera"
+    export function stopCameraScroll() {
+        canCameraScroll = 0
+    }
+
+    /**
+    * Starts a camera scroll.
+    */
+    //% block="scroll camera with vx $horizontally and vy $vertically"
+    //% group="Camera"
+    export function cameraScrollDiagonally(horizontally: number, vertically: number) {
+        canCameraScroll = 1
+        horizontalSpeed = horizontally
+        verticalSpeed = vertically
+    }
+
+    game.onUpdateInterval(10, function () {
+        if (scene.cameraProperty(CameraProperty.X) != 0 && scene.cameraProperty(CameraProperty.Y) != 0) {
+            if (canCameraScroll == 1) {
+                scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + horizontalSpeed, scene.cameraProperty(CameraProperty.Y) + verticalSpeed)
+            }
+        }
+    })
+
+}
